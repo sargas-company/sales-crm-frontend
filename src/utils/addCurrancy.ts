bb9782497754fc140d9e9bd87a2eb1f273dab71d
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2026-04-13 16:32:57 +0300

    remove unusage images

--- src/utils/addCurrancy.ts
+++
@@ -1,3 +0,0 @@
-export default function addCurrancy(amount: string | number) {
-    return `$${amount}`
-}
--- src/utils/genRandomColor.tsx
+++
@@ -1,6 +0,0 @@
-import { colorPallets } from "../theme/colors";
-
-const genRandomColor = () =>
-  colorPallets[Math.ceil(Math.random() * colorPallets.length - 1)].color;
-
-export default genRandomColor;
